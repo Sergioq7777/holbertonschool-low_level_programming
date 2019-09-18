601e52faf8a6eda31ba836d7c78b07a600728499
sergioqm7777 <1230@holbertonschool.com>
2019-09-18 22:10:15 +0000

    correction on the definitions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-int i;
-char c;
-long int l;
-long long ll;
-float f;
-printf("Size of a char is:%d byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int is:%d byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int is:%d byte(s)\n", (unsigned long)sizeof(l));
-printf("Size of a long long int is:%d byte(s)\n", (unsigned long)sizeof(ll));
-printf("Size of a float is:%d byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char is:%d byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int is:%d byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int is:%d byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int is:%d byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a float is:%d byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
