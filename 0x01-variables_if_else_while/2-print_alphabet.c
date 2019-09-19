e212fb6567d5d6ea9bb5965722f2c8f72dff33be
sergioqm7777 <1230@holbertonschool.com>
2019-09-19 22:10:33 +0000

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
