a122455916e792a49fac380ba814771d6ba3ad6a
sergioqm7777 <1230@holbertonschool.com>
2019-09-19 23:56:59 +0000

    adding number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = '1'; b <= '9'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
