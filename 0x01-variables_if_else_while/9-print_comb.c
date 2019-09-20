1af04cec451eb07c4a9b28670dabd22dbe3ef832
sergioqm7777 <1230@holbertonschool.com>
2019-09-20 04:42:09 +0000

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,12 @@ int main(void)
 	for (b = '0'; b <= '9'; b++)
 	{
 		putchar(b);
-		if (b < '9')
+		if (b != '9')
+		{
 		putchar(',');
 		putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
