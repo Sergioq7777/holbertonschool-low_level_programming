93b37b7696f7696371bc490b830c315e3435c5c0
sergioqm7777 <1230@holbertonschool.com>
2019-09-24 16:59:07 +0000

    fixing spaces

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
-
+/**
+ * _abs - check the code for Holberton School students.
+ *@n: ineteger
+ * Return: Always .
+ */
 int _abs(int n)
 
 {
 	if (n < 0)
 	{
-	return (n*-1);
+		return (n*-1);
 	}
-
-        else 
+	else
 	{
 		return (n*1);
 	}
