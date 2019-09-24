cfc91ef4f7824753f3087189c3830623df1d1de6
sergioqm7777 <1230@holbertonschool.com>
2019-09-24 16:08:16 +0000

    adding @c inetiger

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
-
+/**
+ * _isalpha - check the code for Holberton School students.
+ *@c: ineteger 
+ * Return: Always 0.
+ */
 int _isalpha(int c)
+
 {
 	if (c >= 65 && c <= 90)
-{
-	return (1);
-}
+		return (1);
 	if (c >= 97 && c <= 122)
-{
-	return (1);
-}
+		return (1);
 	else
-	{
 		return(0);
 }
 }
