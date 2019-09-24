80dcc789504974015ab209228d85663346d26b90
sergioqm7777 <1230@holbertonschool.com>
2019-09-24 16:50:06 +0000

    fixing spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,25 @@
 #include "holberton.h"
-
+/**
+ * print_sign - check the code for Holberton School students.
+ *@n: ineteger
+ * Return: Always .
+ */
 int print_sign(int n)
 
 {
 	if (n > '0')
-{
-	_putchar ('+');
-	return (1);
+	{
+		_putchar ('+');
+		return (1);
 	}
 	else if (n == '0')
-{
-	_putchar ('0');
-	return (0);
-
-}
+	{
+		_putchar ('0');
+		return (0);
+	}
 	else
 	{
 		_putchar ('-');
 		return (-1);
-		}
+	}
 }
