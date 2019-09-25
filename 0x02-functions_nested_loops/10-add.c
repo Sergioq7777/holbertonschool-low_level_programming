69ea4c34664881823b480fa4d6a856565cd01c72
sergioqm7777 <1230@holbertonschool.com>
2019-09-25 18:48:46 +0000

    using the prototype

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *@a: inetiger
+ *@b: inetiger
+ * Return: Always .
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
