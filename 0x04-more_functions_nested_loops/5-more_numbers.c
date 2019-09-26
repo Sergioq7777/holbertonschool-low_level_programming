f157d25b51290d86db2d002ae24c713f10c4d55c
sergioqm7777 <1230@holbertonschool.com>
2019-09-26 20:00:14 +0000

    corretction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,21 +12,16 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (b = 0 ; b <= 14 ; b++)
+	for (b = 0 ; b <= 10 ; b++)
 	{
 		for (a = 0 ; a <= 14 ; a++)
 		{
-			if (a < 10)
-			{
-				_putchar(a + '0');
-			}
-			else
+			if (a >= 10)
 			{
 				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
 			}
+			_putchar((a % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
