a06329f5aede8faa44e68427ad3a6d8339924ae2
sergioq7777 <sergioq.7777@gmail.com>
2019-10-01 14:18:26 +0000

    first try

---
+++ 0x02-functions_nested_loops/mock.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"holberton.h"
+
+int main()
+{
+	int a;
+	int b;
+	
+
+
+	for (b = 0 ; b <= 10 ; b++)
+		{
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+			printf("%c",a);
+	}
+
+printf("\n");
+		}
+
+
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/mock2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"holberton.h"
+
+int main(void)
+{
+	char b;
+
+	int a;
+
+		for (a = 0 ; a <= 9 ; a++)
+		{
+			printf("%d",a);
+		}
+
+
+
+	for (b = 'a' ; b <= 'f' ; b++)
+		{
+			printf("%c",b);
+		}
+
+	printf("\n");
+}
