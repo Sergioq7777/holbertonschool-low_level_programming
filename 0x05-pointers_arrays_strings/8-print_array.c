82f2d8d5c8228b8dc9605baedc50dd320184506f
sergioq7777 <sergioq.7777@gmail.com>
2019-10-02 20:04:32 +0000

    if option

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *@a: integer
+ *@n: integer
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0 ; x <= n -1; x++)
+	{
+		if (x == n - 1 )
+		{
+			printf("%d", a[x]);
+		}
+		else
+		{
+			printf("%d, ", a[x]);
+		}
+
+	}
+	printf("\n");
+}
