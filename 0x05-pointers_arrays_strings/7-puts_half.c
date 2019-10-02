2bf50f987179a86ed8cfb59764ee6fa07eefcfec
sergioq7777 <sergioq.7777@gmail.com>
2019-10-02 17:16:18 +0000

    adding the else

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts_half - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int c, mh, h, n;
+
+
+	for (c = 0 ; str[c] != '\0' ; ++c)
+		;
+
+	h  = c / 2;
+	mh = c % 2;
+
+	if (mh == 0)
+	{
+		for (; h < c ; ++h)
+		{
+		_putchar(str[h]);
+		}
+	}
+	else
+	{
+		n = (h - 1) / 2;
+		h++;
+		_putchar(n);
+	}
+	_putchar('\n');
+}
