4a1cf34827290111f19a1fd4f6e4dd4b7263a3d8
sergioq7777 <sergioq.7777@gmail.com>
2019-10-04 01:36:14 +0000

    first try

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - check the code for Holberton School students.
+ *@s: char
+ * Return: Void.
+ */
+
+char *cap_string(char *s)
+{
+	int a;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+		if (s[a] == ',' || s[a] == ';' || s[a] == '.' ||
+		    s[a] == '!' || s[a] == '?' || s[a] == '"' ||
+		    s[a] == '(' || s[a] == ')' || s[a] == '{' ||
+		    s[a] == '}' || s[a] == '\n'|| s[a] == ' ' ||)
+		{
+			if (s[a + 1] >= 'a' && s[a + 1] <= 'z') 
+			{
+			s[a + 1] = s[a + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
