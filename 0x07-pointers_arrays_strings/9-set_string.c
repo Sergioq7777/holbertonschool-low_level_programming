216feed95812ced3c872fda4de20286ce32db313
sergioq7777 <sergioq.7777@gmail.com>
2019-10-08 19:14:39 +0000

    really difficult

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,14 +1,1 @@
-#include "holberton.h"
-
-/**
- * set_string - check the code for Holberton School students.
- *@s: char
- *@to: char
- * Return: Void.
- */
-
-void set_string(char **s, char *to)
-{
-	*s = to;
-
-}
+abc123
