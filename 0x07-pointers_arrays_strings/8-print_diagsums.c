e27b1172658761fd5e435c2c57ae6b8e8c120971
sergioq7777 <sergioq.7777@gmail.com>
2019-10-08 19:14:04 +0000

    really difficult

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,25 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * *_strpbrk - check the code for Holberton School students.
- *@a: char
+ * print_diagsums - check the code for Holberton School students.
+ *@a: int
+ *@size: int
  * Return: Void.
  */
 
 void print_diagsums(int *a, int size)
 {
+	int x, y;
+	int sum1 = 0;
+	int sum2 = 0;
 
+	for (x = 0; x < size; x++)
+	{
+		sum1 += a[(size + 1) * x];
+	}
+	for (y = 0; y < size; y++)
+	{
+		sum2 += a[(size - 1) * (y + 1)];
+	}
+	print("%d, %d\n", sum1, sum2)
 }
