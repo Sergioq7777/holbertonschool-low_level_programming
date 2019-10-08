6134dfe3304b4f64885beca0d237e570a576a2d7
sergioq7777 <sergioq.7777@gmail.com>
2019-10-08 19:13:02 +0000

    really difficult

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != *needle && *haystack != '\0' ; haystack++)
-		;
-	if (*haystack == *needle)
+	int th, tn;
+
+	for (th = 0 ; haystack[th] != '\0' ; th++)
 	{
-		return (haystack);
+		for (tn = 0 ; needle[tn] != '\0' ; tn++)
+		{
+			if (haystack[th + tn] != needle[tn])
+			{
+				break;
+			}
+		}
+		if (!needle[tn])
+		{
+			return (haystack + th);
+		}
 	}
-	else
-	{
-		return ('\0');
-	}
+	return ('\0');
 }
