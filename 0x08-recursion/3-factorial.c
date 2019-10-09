1a9f59658cca0b5064742123ad4c4a60fbcccee4
sergioq7777 <sergioq.7777@gmail.com>
2019-10-09 20:59:23 +0000

    if plus else if plus else

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,16 @@
 
 int factorial(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
+	else if (n == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		n = n * factorial(n - 1);
+		return (n * factorial(n - 1));
 	}
-	return (n);
 }
