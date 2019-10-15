09555317b1226e56befd5ed8831281337d4a9bb7
sergioqm7777 <1230@holbertonschool.com>
2019-10-15 19:59:00 +0000

    Requirements

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @c: char
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+
+
+}
