3db89512e574afb2bda121524d0367902f7a2744
sergioqm7777 <1230@holbertonschool.com>
2019-10-15 04:28:11 +0000

    PLD rewiuw

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,51 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
+#include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *@argc: int
- *@argv: char
- * Return: Always.
+ * main - Is the main code of arg
+ * @argc: The size 0 of argv
+ * @argv: Array that pointers just the project name
+ * Return: 0 (Always success)
  */
 
 int main(int argc, char *argv[])
 {
-	int change, coins, i = 0;
-	char denom[] = {25, 10, 5, 2, 1};
-
-	if (argc < 2)
+	int cents = 0;
+	int money;
+	if (argc != 2)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	else
+	money = atoi(argv[1]);
+	while (money > 0)
 	{
-		for (i = 1; i < argc; i++)
+		if (money - 25 >= 0)
 		{
-			for (x = 0 ; argv[i][x] != '\0' ; x++)
-			{
-				if (argv[i][x] < 47 || argv[i][x] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			y +=  atoi(argv[i]);
+			money = money - 25;
+			cents++;
 		}
-		printf("%d\n", y);
-		return (0);
+		else if (money - 10 >= 0)
+		{
+			money = money - 10;
+			cents++;
+		}
+		else if (money - 5 >= 0)
+		{
+			money = money - 5;
+			cents++;
+		}
+		else if (money - 2 >= 0)
+		{
+			money = money - 2;
+			cents++;
+		}
+		else if (money - 1 >= 0)
+		{
+			money = money - 1;
+			cents++;
+		}
+
 	}
+	printf("%d\n", cents);
+	return (0);
 }
