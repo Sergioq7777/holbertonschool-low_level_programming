b05b50b4a9a608ed4b55fbf13e37cf288ea743c8
sergioq7777 <sergioq.7777@gmail.com>
2019-10-17 17:59:02 +0000

    errase

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,93 @@
+#include "holberton.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+/**
+
+ * alloc_grid - createa matrix with malloc.
+
+ * @width: param of the pointer of the string
+
+ * @height:THe height of the matrix
+
+ * Return: The matrix.
+
+ */
+
+
+
+int **alloc_grid(int width, int height)
+
+{
+
+	int **matrix;
+
+	int x, y;
+
+
+
+	if (width <= 0 || height <= 0)
+
+	{return (NULL);
+
+
+
+	}
+
+	matrix = malloc(height * sizeof(int *));
+
+
+
+	if (matrix == NULL)
+
+	{
+
+		return (NULL);
+
+	}
+
+
+
+	for (x = 0; x < height; x++)
+
+	{
+
+		matrix[x] = malloc(width * sizeof(int));
+
+		if (matrix[x] == NULL)
+
+		{
+
+			for (x = width; x >= 0; x--)
+
+			{
+
+				free(matrix[x]);
+
+			}
+
+			free(matrix);
+
+			return (NULL);
+
+		}
+
+
+
+		for (y = 0; y < width; y++)
+
+		{
+
+			matrix[x][y] = 0;
+
+		}
+
+	}
+
+	return (matrix);
+
+}
