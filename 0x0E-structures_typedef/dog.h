89ccd0f28bdc849416cbd5fbf1a9205541bf026a
sergioq7777 <sergioq.7777@gmail.com>
2019-10-21 23:53:06 +0000

    adding the first part

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
-#include
+#ifndef DOG
+#define DOG
+
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+ * struct dog - dog object
+ * @name: name
+ * @age: float
+ * @owner: owner
+ * Description: characteristics for dog
+ **/
+
+struct dog
+{
+	char *name;
+	float  age;
+	char *owner;
+};
+
+#endif
