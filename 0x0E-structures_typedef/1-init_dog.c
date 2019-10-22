10e276137ebe87e578cbfa63800f68c8eb86f2bf
sergioqm7777 <1230@holbertonschool.com>
2019-10-22 03:50:12 +0000

    miss everything

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - Initialize a variable of type struct dog.
  *@d: struct
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Initialize a variable of type struct dog.
+ *@d: struct
+ *
+ * Return: Always 0.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+
+}
