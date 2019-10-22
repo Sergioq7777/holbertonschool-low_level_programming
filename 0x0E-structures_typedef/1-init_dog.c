8d5b4633b91d99f290fbc0f2e9e3ba0e3c9c60dc
sergioq7777 <sergioq.7777@gmail.com>
2019-10-22 00:13:31 +0000

    finish

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <dog.h>
+/**
+ * main - check the code for Holberton School students.
+ *@d: struct
+ *@name: char
+ *@age: float
+ *@owner: char
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if()
+	{
+
+
+	}
+	else 
+}
