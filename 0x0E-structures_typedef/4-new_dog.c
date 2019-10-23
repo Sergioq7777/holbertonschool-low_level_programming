94e9600be92334edec9319cdb5b21b04dc619062
sergioqm7777 <1230@holbertonschool.com>
2019-10-23 04:05:10 +0000

    changin the logic

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 /**
  * print_dog - Initialize a variable of type struct dog.
- *@d: struct
- *
+ *@name: char
+ *@age: float
+ *@owner: char
  * Return: Always 0.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *p;
 
 
+	p = malloc(sizeof(dog_t));
+
+	if (p == NULL)
+		return (NULL);
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
 }
