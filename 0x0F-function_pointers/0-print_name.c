6c9be5cbb5579f9531c60369f7cc150ffe33fbfc
sergioq7777 <sergioq.7777@gmail.com>
2019-10-23 19:54:40 +0000

    the prototype, calls the other functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -11,7 +12,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
+	if (name == NULL || f == NULL)
 	{
 		return;
 	}
