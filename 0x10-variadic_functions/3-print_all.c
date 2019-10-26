ed0a8ad343db276b3f8f18491cd4c1e5736a464f
sergioq7777 <sergioq.7777@gmail.com>
2019-10-26 00:08:42 +0000

    miss

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+
+/**
+ * print_string - function that prints number, followed by a new line.
+ *@n: unsigned int.
+ *@x: unsigned int
+ *
+ * Return: Always .
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+	
+}
