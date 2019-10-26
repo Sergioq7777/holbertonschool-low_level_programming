6dadfebc864f0731417baa694b680fd9815273d6
sergioqm7777 <1230@holbertonschool.com>
2019-10-26 23:41:38 +0000

    fixing betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,25 +1,25 @@
 #include "variadic_functions.h"
 
 /**
- * print_number - function that prints number, followed by a new line.
+ * print_numbers - function that prints number, followed by a new line.
  *@n: unsigned int.
- *@x: unsigned int
+ *@separator: char
  *
- * Return: Always .
+ * Return: No return .
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 
-	va_list arguments;
-	unsigned int x ;
+	va_list arguments;/** va_list arguments pointer varibles */
+	unsigned int x;
 
 
-	va_start(arguments, n);
+	va_start(arguments, n);/** va_start initializes the argument pointer */
 
 	for (x = 0; x < n; x++)
 	{
-		printf("%d", (va_arg(arguments, unsigned int)));
+		printf("%d", (va_arg(arguments, unsigned int)));/**return next argumment */
 
 		if ((x != (n - 1)) && separator != NULL)
 		{
