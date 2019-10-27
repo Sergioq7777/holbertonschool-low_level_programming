02f83c3780a5fd76140d9bd714702054983731af
sergioqm7777 <1230@holbertonschool.com>
2019-10-27 20:46:25 +0000

    adding second conditional

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 
 /**
- * print_string - function that prints number, followed by a new line.
- *@n: unsigned int.
- *@x: unsigned int
+ * print_strings - function that prints number, followed by a new line.
+ *@separator: char.
+ *@n: unsigned int
  *
  * Return: Always .
  */
@@ -17,9 +17,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(arguments, n);
 
-	for (x = 0; x < n; x+)
+	for (x = 0; x < n; x++)
 	{
-		y = (va_arg(arguments, char *));
+		y = va_arg(arguments, char *);
 
 		if (y != NULL)
 		{
@@ -27,13 +27,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("(nil)");
+			printf("(nil)");/**if one of the string isNULL print. */
 		}
 		if ((x != (n - 1)) && separator != NULL)
-		{
+		{/**if the separator is null don't print it*/
 			printf("%s", separator);
 		}
+		va_end(arguments);
 	}
-	printf("\n");
-	va_end(arguments);
+	printf("\n");/** Print a new line at the end of the function*/
 }
