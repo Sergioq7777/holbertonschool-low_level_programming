673d045fea59a9531a53c5a78d8c6317d32b581d
sergioqm7777 <1230@holbertonschool.com>
2019-11-05 21:20:51 +0000

    ()on the pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a alist.
+ *@head: type listint_t
+ *
+ * Return: Always 0.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+}
