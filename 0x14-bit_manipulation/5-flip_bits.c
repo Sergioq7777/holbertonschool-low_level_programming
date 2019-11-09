e5cec9dd079c01170d22a222f5029d9db7aadd2f
sergioqm7777 <1230@holbertonschool.com>
2019-11-09 00:36:43 +0000

    fix

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * unsigned int - function that returns the number of bits - to flip.
+ * @n: unsigned int.
+ * @m:unsigned int.
+ *
+ * Return: int.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num, tmp;
+
+	tmp = n ^ m;
+	num = 0;
+
+	while (tmp)
+	{
+		num = num + (tmp & 1);
+		tmp >>= 1;
+	}
+	return (num);
+}
