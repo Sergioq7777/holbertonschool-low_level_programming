03966a45cf1d4fbbd3cbd6367a44ac50442605d7
sergioq7777 <sergioq.7777@gmail.com>
2019-11-12 19:48:13 +0000

    Fixin betty and adding stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * read_textfile-Function that reads file and prints it to the stndout.
+ *
+ *@filename: char.
+ *@letters: char.
+ *
+ * Return: ssize_t.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int fd;
+	ssize_t nr_bytes;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	buffer = malloc(sizeof(letters));
+
+	if (!buffer)
+		return (0);
+
+	if (fd == -1)
+		return (0);
+
+	else
+	{
+		nr_bytes = read(fd, buffer, letters);
+		close(fd);
+
+		write(1, buffer, letters);
+	}
+
+	return (nr_bytes);
+}
