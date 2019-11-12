affa71ea28374613b9cb3aa4fdb737c1757cdef6
sergioq7777 <sergioq.7777@gmail.com>
2019-11-12 20:52:36 +0000

    finish

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * create_file - Function that ccreates a file.
+ *
+ *@filename: char.
+ *@text_content: char.
+ *
+ * Return: int.
+ */
+
+int create_file(const char *filename, char *text_content);
+{
+
+	int fd;
+	ssize_t 
+
+
+
+}
