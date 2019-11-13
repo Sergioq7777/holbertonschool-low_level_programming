58796e1440cbeb53a9c9dbfbb0b690560002ef50
sergioqm7777 <1230@holbertonschool.com>
2019-11-13 04:00:33 +0000

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,25 @@
  * Return: int.
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 
-	int fd;
-	ssize_t 
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	int cr;
 
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
+	for (cr = 0; text_content[cr]; cr++)
+		;
 
+	if (fd == -1)
+		return (-1);
+	cr = write(fd, text_content, cr);
+
+	close(fd);
+
+	return (1);
 }
