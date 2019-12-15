343e12f2025cfaa5a7bb2844fd422329d63cf570
sergioq7777 <sergioq.7777@gmail.com>
2019-12-15 05:50:46 +0000

    #1 Add

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,5 +6,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t cnt = 0;
 
+	if (h == NULL)
+		return (cnt);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
 }
