132678b827de247d59b68f38b5cbb7e9b5c21871
sergioq7777 <sergioq.7777@gmail.com>
2019-12-16 02:36:26 +0000

    ADD

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 /**
- * print_dlistint - print dlistint_t
+ * dlistint_len - Count doubly link.
  *@h: head
  * Return: Always nodes.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t cnt = 0;
 
+	while (h)
+	{
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
 }
