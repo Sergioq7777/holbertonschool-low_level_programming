ded5c203c10c654dc23bca7b1c17a540971f2d65
sergioq7777 <sergioq.7777@gmail.com>
2019-12-16 03:04:34 +0000

    Fixing betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,36 @@
 #include "lists.h"
 /**
- * print_dlistint - print dlistint_t
- *@h: head
+ * *add_dnodeint - add node to..
+ *@head: head
+ *@n: number
  * Return: Always nodes.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *ptr;
 
+	if (head == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(dlistint_t));
+	if (!ptr)
+	{
+		return (NULL);
+	}
+
+	if (*head == NULL)
+	{
+		ptr->n = n;
+		ptr->next = *head;
+		ptr->prev = NULL;
+		*head = ptr;
+	}
+	else
+	{
+		ptr->n = n;
+		ptr->next = *head;
+		ptr->prev = NULL;
+		(*head)->prev = ptr;
+		*head = ptr;
+	}
+	return (*head);
 }
