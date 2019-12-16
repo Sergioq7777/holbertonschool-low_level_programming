474b9faafbe2c1dd759095df5b3baa1193c74bc2
sergioq7777 <sergioq.7777@gmail.com>
2019-12-16 04:11:28 +0000

    Correction tmp

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,5 +6,15 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *tmp;
+	unsigned int cnt = 0;
+
+	while (head && cnt < index)
+	{
+		head = head->next;
+		cnt++;
+	}
+	tmp = head;
+	return (tmp);
 
 }
