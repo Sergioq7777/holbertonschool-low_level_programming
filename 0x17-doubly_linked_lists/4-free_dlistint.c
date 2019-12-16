490b360e96379a8b21a22f49f4e0d52311d8f041
sergioq7777 <sergioq.7777@gmail.com>
2019-12-16 03:46:06 +0000

    Create a temporari

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 /**
- * print_dlistint - print dlistint_t
- *@h: head
+ * free_dlistint - Free the linked list
+ *@head: head
  * Return: Always nodes.
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *tmp;
 
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
