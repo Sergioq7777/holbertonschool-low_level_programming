a6f37dc0375ac61870c28ce77dcba274efe9db63
sergioq7777 <sergioq.7777@gmail.com>
2020-01-19 19:11:30 -0500

    new hash- betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,21 +5,21 @@
  *
  * Return: Pointer to new hash table
  */
-hash_table_t *hash_table_create(unsigned long int size)
+hash_tables_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *new_hash_;
-        unsigned int value;
+	hash_table_t *new_hash_;
+	unsigned int value;
 
-        new_hash_ = malloc(sizeof(hash_table_t));
-        if(!new_hash_)
-                return (NULL);
-        new_hash_->size = size;
-	    new_hash_->array =  malloc(sizeof(hash_node_t *) * (size));
-	    if (!new_hash_->array)
-		    return (NULL);
-	    for (value = 0; value < size; value++)
-	    {
-		    new_hash_->array[value] = NULL;
-	    }
-	    return (new_hash_);
+	new_hash_ = malloc(sizeof(hash_table_t));
+	if (!new_hash_)
+		return (NULL);
+	new_hash_->size = size;
+	new_hash_->array =  malloc(sizeof(hash_node_t *) * (size));
+	if (!new_hash_->array)
+		return (NULL);
+	for (value = 0; value < size; value++)
+	{
+		new_hash_->array[value] = NULL;
+	}
+	return (new_hash_);
 }
