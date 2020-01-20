8fc343fa7887938b361a8026b87e1aa0c3cf8bff
sergioq7777 <sergioq.7777@gmail.com>
2020-01-19 19:45:23 -0500

    better emacs

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,25 +1,26 @@
-#include "hash.h"
+#include "hash_tables.h"
 /**
  * hash_table_create - creates a hash table.
  * @size: Size.
  *
  * Return: Pointer to new hash table
  */
-hash_tables_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *new_hash_;
-	unsigned int value;
 
-	new_hash_ = malloc(sizeof(hash_table_t));
-	if (!new_hash_)
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash;
+	unsigned int value = 0;
+
+	new_hash =  malloc(sizeof(hash_table_t));
+	if (!new_hash)
 		return (NULL);
-	new_hash_->size = size;
-	new_hash_->array =  malloc(sizeof(hash_node_t *) * (size));
-	if (!new_hash_->array)
+	new_hash->size = size;
+	new_hash->array =  malloc(sizeof(hash_node_t *) * (size));
+	if (!new_hash->array)
 		return (NULL);
 	for (value = 0; value < size; value++)
 	{
-		new_hash_->array[value] = NULL;
+		new_hash->array[value] = NULL;
 	}
-	return (new_hash_);
+	return (new_hash);
 }
