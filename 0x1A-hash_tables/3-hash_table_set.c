b04f2c12906e2d00cd815007eb9644f7ce99e937
sergioq7777 <sergioq.7777@gmail.com>
2020-01-20 21:20:03 -0500

    unsolved

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,76 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - adds to hash table
+ *
+ *@ht: hash table
+ *@key: key.
+ *@value: value
+ *Return: 1 True
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *add_h;
+	unsigned long int ind = 0;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	add_h = malloc(sizeof(hash_node_t));
+	if (!add_h)
+		return (0);
+	ind = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[ind] == NULL)
+	{
+		add_h->key = strdup(key);
+		if (!add_h->key)
+			return (0);
+		add_h->value = strdup(value);
+		if (!add_h->value)
+			return (0);
+		add_h->next = NULL;
+		ht->array[ind] = add_h;
+		return (1);
+	}
+	free(add_h);
+	return (h_t_s(ht, key, value));
+}
+/**
+ *h_t_s - adds hash table
+ *
+ *@ht: hash
+ *@key: Key
+ *@value: value key
+ *Return: 1 True
+ */
+int h_t_s(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *aux, *add_h;
+	unsigned long int ind = 0;
+
+	add_h = malloc(sizeof(hash_node_t));
+	if (!add_h)
+		return (0);
+	ind = key_index((const unsigned char *)key, ht->size);
+	aux =  ht->array[ind];
+	while (aux)
+	{
+		if (strcmp(key, aux->key) == 0)
+		{
+			free(aux->value);
+			aux->value = strdup(value);
+			if (!aux->value)
+				return (0);
+			return (1);
+		}
+		aux = aux->next;
+	}
+	aux = ht->array[ind];
+	add_h->key = strdup(key);
+	if (!add_h->key)
+		return (0);
+	add_h->value = strdup(value);
+	if (!add_h->value)
+		return (0);
+	add_h->next = aux;
+	ht->array[ind] = add_h;
+	return (1);
+}
